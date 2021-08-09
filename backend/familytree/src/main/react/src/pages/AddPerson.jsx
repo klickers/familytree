cca3398a9096d5ebbb40ce62f1f8211100f8d6c2
Catherine Chen <bluebirid228@gmail.com>
2021-08-08 21:37:17 -0700

    Update AddPerson.jsx

--- backend/familytree/src/main/react/src/pages/AddPerson.jsx
+++ backend/familytree/src/main/react/src/pages/AddPerson.jsx
@@ -1,9 +1,6 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom'
+import React, { useState } from 'react'
 
 function AddPerson() {
-
-    const { pid } = useParams()
 
     const [state, setState] = useState({
         fname: '',
@@ -12,6 +9,7 @@ function AddPerson() {
         dob: '',
         lod: '',
         dod: '',
+        gender: 'Male',
         relation: '',
     });
 
@@ -22,8 +20,6 @@ function AddPerson() {
     }
 
     function handleSubmit (event) {
-        alert('A form was submitted: ' + state);
-
         console.log(JSON.stringify(state))
 
         fetch(`/api/v1/person`, 
@@ -34,86 +30,68 @@ function AddPerson() {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-            }).then(function(response) {
-                console.log(response)
-                window.location.href = '/'
-            });
-        
-        if (pid) {
-
-        }
+            })
+            .then(res => res.text())
+            .then((data) => {
+                let newPid = data.split(": ").pop()
+                window.location.href = `/profile/${newPid}`
+            })
 
         event.preventDefault();
     }
   
-    //render() {
-        const { fname, lname, lob, dob, lod, dod, relation } = state;
-        return (
-            <div className="addPerson">
-                <div className="box">
-                    <h1>Add Person</h1>
-                    <form onSubmit={handleSubmit}>
-                        <input 
-                            type="text" 
-                            name="fname" 
-                            placeholder="First Name"
-                            value={fname}  
-                            onChange={handleChange} />
-                        <input 
-                            type="text" 
-                            name="lname" 
-                            placeholder="Last Name"
-                            value={lname}  
-                            onChange={handleChange} />
-                        <select name="gender" id="gender" onChange={handleChange}>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                        </select>
-                        <input 
-                            type="text" 
-                            name="lob" 
-                            placeholder="Birthplace"
-                            value={lob}  
-                            onChange={handleChange} />
-                        <input 
-                            type="text" 
-                            name="dob" 
-                            placeholder="Birthdate"
-                            value={dob}  
-                            onChange={handleChange} />
-                        <input 
-                            type="text" 
-                            name="lod" 
-                            placeholder="Deathplace"
-                            value={lod}  
-                            onChange={handleChange} />
-                        <input 
-                            type="text" 
-                            name="dod" 
-                            placeholder="Deathdate"
-                            value={dod}  
-                            onChange={handleChange} />
-                        
-                        
-
-                        <button type="submit">Add Person</button>
-                    </form>
-                </div>
+    const { fname, lname, lob, dob, lod, dod, gender } = state;
+    return (
+        <div className="addPerson">
+            <div className="box">
+                <h1>Add Person</h1>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="text" 
+                        name="fname" 
+                        placeholder="First Name"
+                        value={fname}  
+                        onChange={handleChange} />
+                    <input 
+                        type="text" 
+                        name="lname" 
+                        placeholder="Last Name"
+                        value={lname}  
+                        onChange={handleChange} />
+                    <select name="gender" id="gender" value={gender} onChange={handleChange}>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                    </select>
+                    <input 
+                        type="text" 
+                        name="lob" 
+                        placeholder="Birthplace"
+                        value={lob}  
+                        onChange={handleChange} />
+                    <input 
+                        type="text" 
+                        name="dob" 
+                        placeholder="Birthdate"
+                        value={dob}  
+                        onChange={handleChange} />
+                    <input 
+                        type="text" 
+                        name="lod" 
+                        placeholder="Deathplace"
+                        value={lod}  
+                        onChange={handleChange} />
+                    <input 
+                        type="text" 
+                        name="dod" 
+                        placeholder="Deathdate"
+                        value={dod}  
+                        onChange={handleChange} />
+                    <button type="submit">Add Person</button>
+                </form>
             </div>
-        );
-    //}
+        </div>
+    );
 }
 
-/*
-<select name="relation" id="relation" onChange={handleChange}>
-                        <option value="">Relation</option>
-                        <option value="Husband">Husband</option>
-                        <option value="Wife">Wife</option>
-                        <option value="Father">Father</option>
-                        <option value="Mother">Mother</option>
-                        <option value="Son">Son</option>
-                        <option value="Daughter">Daughter</option>
-                    </select>
- */
 
 export default AddPerson
