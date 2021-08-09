be26626de510bb7212fef64f81340270edaa3147
Catherine Chen <bluebirid228@gmail.com>
2021-08-09 14:14:21 -0700

    add credits

--- backend/familytree/src/main/react/src/components/Footer.jsx
+++ backend/familytree/src/main/react/src/components/Footer.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 function Footer() {
     return (
         <footer>
-            <p>Made by XXX.</p>
+            <p>Made by <a href="https://github.com/klickers" target="_blank">Catherine Chen</a> and <a href="https://github.com/torashad99" target="_blank">Mohammed Rashad</a>.</p>
+            <p>Mentored by <a href="https://www.linkedin.com/in/spencer-grossarth-642327196/" target="_blank">Spencer Grossarth</a></p>
             <p><a href="https://github.com/klickers/familytree" target="_blank">View on Github</a> | <a href="https://icons8.com/">Icons by Icons8</a></p>
         </footer>
     );
