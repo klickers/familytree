4adb883e5c1b23f78f9dce4e58b73174e03bda89
Catherine Chen <bluebirid228@gmail.com>
2021-08-09 08:08:09 -0700

    add relative functionality

--- backend/familytree/src/main/react/src/pages/AddRelative.jsx
+++ backend/familytree/src/main/react/src/pages/AddRelative.jsx
@@ -12,10 +12,17 @@ function AddRelative() {
         dob: '',
         lod: '',
         dod: '',
+        gender: 'Male',
         relation: '',
         relative: pid,
 
-        hidden: "",
+        original: '',
+        hidden: "hidden",
+        hido: "hidden",
+
+        fandlmatches: [],
+        fmatches: [],
+        lmatches: [],
     });
 
     function handleChange (event) {
@@ -25,18 +32,7 @@ function AddRelative() {
     }
 
     function checkExists (event) {
-        setState(prevState => {
-            return {...prevState, [hidden]: "hidden" };
-        });
-        event.preventDefault()
-    }
-
-    function handleSubmit (event) {
-        alert('A form was submitted: ' + state);
-
-        console.log(JSON.stringify(state))
-
-        fetch(`/api/v1/person`, 
+        fetch(`/api/v1/person/exists`, 
             {
                 method: 'POST',
                 // We convert the React state to JSON and send it as the POST body
@@ -44,20 +40,146 @@ function AddRelative() {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-            }).then(function(response) {
-                console.log(response)
-                window.location.href = '/'
+            })
+            .then(res => res.json())
+            .then((res) => {
+                console.log(res)
+                // if there are matches
+                if (res.fandlmatches && (res.fandlmatches.length !== 0 || res.fmatches.length !== 0 || res.lmatches.length !== 0)) {
+                    setState(prevState => {
+                        return {
+                            ...prevState,
+                            original: 'hidden',
+                            hidden: "",
+                            fandlmatches: res.fandlmatches,
+                            fmatches: res.fmatches,
+                            lmatches: res.lmatches,
+                        };
+                    });
+                }
+                // if there aren't matches
+                else {
+                    setState(prevState => {
+                        return {
+                            ...prevState, 
+                            original: 'hidden',
+                            hido: ""
+                        };
+                    });
+                }
             });
-        
-        if (pid) {
+        event.preventDefault()
+    }
 
+    function addExistingRelation(relId) {
+        if (state.relation == 'mother' || state.relation == 'father') 
+        {
+            let object = {
+                aID: relId,
+                relation: state.relation
+            }
+            fetch(`/api/v1/person/ancestors/${pid}`, 
+                {
+                    method: 'POST',
+                    body: JSON.stringify(object),
+                    headers: {
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    },
+                })
+            .then(res => res.text())
+            .then((response) => {
+                    console.log(response)
+                    window.location.href = '/profile/' . pid
+                });
         }
+        else if (state.relation == 'spouse') 
+        {
+            let object = {
+                sID: relId,
+                relation: state.relation
+            }
+            fetch(`/api/v1/person/spouses/${pid}`, 
+                {
+                    method: 'POST',
+                    body: JSON.stringify(object),
+                    headers: {
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    },
+                })
+            .then(res => res.text())
+            .then((response) => {
+                    console.log(response)
+                    window.location.href = '/profile/' . pid
+                });
+        }
+        else if (state.relation == 'children') // || state.relation == 'grandchildren') 
+        {
+            let object = {
+                dID: relId,
+                relation: state.relation
+            }
+            fetch(`/api/v1/person/descendants/${pid}`, 
+                {
+                    method: 'POST',
+                    body: JSON.stringify(object),
+                    headers: {
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    },
+                })
+            .then(res => res.text())
+            .then((response) => {
+                    console.log(response)
+                    window.location.href = '/profile/' . pid
+                });
+        }
+    }
+
+    function revealHido (event) {
+        setState(prevState => {
+            return {
+                ...prevState, 
+                hidden: 'hidden',
+                hido: ""
+            };
+        });
+        event.preventDefault()
+    }
+
+    function handleSubmit (event) {
+        alert('A form was submitted: ' + state);
+
+        console.log(JSON.stringify(state))  // get rid of
+
+        fetch(`/api/v1/person/with-relative/${state.relation}/${pid}`, 
+            {
+                method: 'POST',
+                body: JSON.stringify(state),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then(res => res.text())
+            .then((response) => {
+                console.log(response)
+                // getting Pid
+                let newPid = response.split(": ").pop()  // remove Pid :
+                var index = newPid.indexOf("\n")
+                if (index === -1) index = undefined
+                newPid = newPid.substring(0, index)  // remove second line
+
+                console.log(newPid)
+                addExistingRelation(newPid)
+                window.location.href = '/profile/' . pid
+            });
 
         event.preventDefault();
     }
   
-    //render() {
-    const { fname, lname, lob, dob, lod, dod, relation, relative, hidden } = state;
+    
+    const { fname, lname, lob, dob, lod, dod, gender, relation, relative, original, hidden, hido, fandlmatches, fmatches, lmatches } = state;
     
     return (
         <div className="addPerson">
@@ -66,7 +188,7 @@ function AddRelative() {
                 &#x2190; Back to Profile
             </Link>
 
-            <div className={`box`}>
+            <div className={`box ${original}`}>
                 <h1>Add Relative</h1>
                 <form onSubmit={checkExists}>
                     <input 
@@ -74,44 +196,58 @@ function AddRelative() {
                         name="fname" 
                         placeholder="First Name"
                         value={fname}  
-                        onChange={handleChange} />
+                        onChange={handleChange}
+                        required />
                     <input 
                         type="text" 
                         name="lname" 
                         placeholder="Last Name"
                         value={lname}  
-                        onChange={handleChange} />
-                    <select name="relation" id="relation" onChange={handleChange}>
+                        onChange={handleChange}
+                        required />
+                    <select name="relation" id="relation" value={relation} onChange={handleChange}>
                         <option value="">Relation</option>
-                        <option value="Husband">Husband</option>
-                        <option value="Wife">Wife</option>
-                        <option value="Father">Father</option>
-                        <option value="Mother">Mother</option>
-                        <option value="Son">Son</option>
-                        <option value="Daughter">Daughter</option>
+                        <option value="spouse">Spouse</option>
+                        <option value="father">Father</option>
+                        <option value="mother">Mother</option>
+                        <option value="children">Child</option>
+                        {/*<option value="grandfather">Grandfather</option>
+                        <option value="grandmother">Grandmother</option>
+                        <option value="grandchildren">Grandchild</option>*/}
                     </select>
                     <button type="submit">Next &#10142;</button>
                 </form>
             </div>
 
-            <div className={`box`}>
+            <div className={`box ${hidden}`}>
                 <p>The relative you've entered may already exist!  Does any of this information look familiar to you?</p>
                 <ul>
-                    <li>
-                        <strong>FirstName LastName</strong> (GENDER born in LOB on DOB)
-                        <br />
-                        <a href="/">+ add as RELATION</a>
-                    </li>
-                    <li>
-                        <strong>Catherine Chen</strong> (female born in California, USA on 04-27-2003)
-                        <br />
-                        <a href="/">+ add as Daughter</a>
-                    </li>
+                    {fandlmatches.map(fl => (
+                        <li>
+                            <strong>{fl.fname} {fl.lname}</strong> ({fl.gender} born in {fl.lob} on {fl.dob})
+                            <br />
+                            <a href="#" onClick={() => addExistingRelation(fl.pid)}>+ add as {relation}</a>
+                        </li>
+                    ))}
+                    {fmatches.map(fl => (
+                        <li>
+                            <strong>{fl.fname} {fl.lname}</strong> ({fl.gender} born in {fl.lob} on {fl.dob})
+                            <br />
+                            <a href="#" onClick={() => addExistingRelation(fl.pid)}>+ add as {relation}</a>
+                        </li>
+                    ))}
+                    {lmatches.map(fl => (
+                        <li>
+                            <strong>{fl.fname} {fl.lname}</strong> ({fl.gender} born in {fl.lob} on {fl.dob})
+                            <br />
+                            <a href="#" onClick={() => addExistingRelation(fl.pid)}>+ add as {relation}</a>
+                        </li>
+                    ))}
                 </ul>
-                <button type="submit">None of These &#10142;</button>
+                <button onClick={revealHido}>None of These &#10142;</button>
             </div>
 
-            <div className={`box ${hidden}`}>
+            <div className={`box ${hido}`}>
                 <h1>Add Relative</h1>
                 <form onSubmit={handleSubmit}>
                     <input 
@@ -133,7 +269,7 @@ function AddRelative() {
                         value={relative}
                         readOnly
                         hidden />
-                    <select name="gender" id="gender" onChange={handleChange}>
+                    <select name="gender" id="gender" value={gender} onChange={handleChange}>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                     </select>
@@ -167,11 +303,6 @@ function AddRelative() {
 
         </div>
     );
-    //}
 }
-
-/*
-
- */
 
 export default AddRelative
